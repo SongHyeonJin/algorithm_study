a4a30c1a01060713b24155cfcc8ca4a7cfdf13f5
HyeonJin Song <shwj1602@naver.com>
2024-06-16 23:36:14 +0900

    Feat : searching 유형(장난꾸러기, 좌표 정렬)

---
+++ src/searching/Lec49.java
@@ -0,0 +1,30 @@
+package searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Lec49 {
+    public ArrayList<Integer> solution(int n, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] temp = arr.clone();
+        Arrays.sort(temp);
+        for(int i=0; i<n; i++){
+            if(arr[i] != temp[i]) answer.add(i+1);
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        Lec49 T = new Lec49();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        for (int x : T.solution(n, arr)) System.out.print(x+" ");
+    }
+}
---
+++ src/searching/Lec50.java
@@ -0,0 +1,39 @@
+package searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Lec50 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Point> arrayList = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arrayList.add(new Point(x, y));
+        }
+        Collections.sort(arrayList);
+        for (Point o : arrayList)
+            System.out.println(o.x+" "+o.y);
+    }
+}
+class Point implements Comparable<Point> {
+    public int x, y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if(this.x == o.x) return this.y - o.y;
+        else return this.x - o.x;
+    }
+}
