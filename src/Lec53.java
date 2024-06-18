b77f768ace678bc858c8df85dba1bca59d68697a
HyeonJin Song <shwj1602@naver.com>
2024-06-19 06:59:20 +0900

    Feat : searching 유형(마구간 정하기)

---
+++ src/Lec53.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Lec53 {
+    public int count(int[] arr, int dist){
+        int cnt = 1;
+        int ep = arr[0];
+        for (int i=1; i<arr.length; i++){
+            if(arr[i]-ep>=dist){
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
+    }
+    public int solution(int n, int c, int[] arr){
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n-1];
+        while (lt<=rt){
+            int mid = (lt+rt)/2;
+            if(count(arr, mid)>=c){
+                answer = mid;
+                lt = mid+1;
+            }
+            else rt = mid-1;
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        Lec53 T = new Lec53();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        System.out.println(T.solution(n, c, arr));
+    }
+}
